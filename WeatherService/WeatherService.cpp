db33183a4154ee3306f512557a42d75083be454a
”Anna <annyastar1607@gmail.com>
2025-04-23 22:32:04 +0300

    Добавлена функция main для XmlService

--- WeatherService/WeatherService.cpp
+++ WeatherService/WeatherService.cpp
@@ -1,1 +1,10 @@
-﻿
+﻿#include "XmlService.h"
+#include "Weather.h"
+#include <iostream>
+
+int main() {
+    XmlService xmlService;
+    Weather w = xmlService.getWeather("weather.xml"); 
+    w.printWeather();
+    return 0;
+}
